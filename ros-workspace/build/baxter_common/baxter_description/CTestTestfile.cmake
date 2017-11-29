21cc084ac6003bff3a82627bd9da37328a1c2f0e
Kevin Luo <klevin.gluo@gmail.com>
2017-11-29 13:04:03 -0500

    adding git ignore and removing build and devel files

--- ros-workspace/build/baxter_common/baxter_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/redbeans/robotics/ros-workspace/src/baxter_common/baxter_description
-# Build directory: /home/redbeans/robotics/ros-workspace/build/baxter_common/baxter_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros-workspace/build/baxter_examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/redbeans/robotics/ros-workspace/src/baxter_examples
-# Build directory: /home/redbeans/robotics/ros-workspace/build/baxter_examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
